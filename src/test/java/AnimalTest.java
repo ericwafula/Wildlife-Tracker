c5002cfc6bd51592819786f514e99ee6febfe061
ericwafula <eric.wafula@moringaschool.com>
2021-08-15 12:36:39 +0300

    added getName testCase

--- src/test/java/AnimalTest.java
+++ src/test/java/AnimalTest.java
@@ -1,5 +1,6 @@
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class AnimalTest {
@@ -9,5 +10,10 @@ public class AnimalTest {
         assertTrue(lucky instanceof Animal);
     }
 
-
+    @Test
+    public void getName_animalInstantiatesWithName_string() {
+        Animal lucky = new Animal("Lucky", 1);
+        lucky.getName();
+        assertEquals("Lucky", lucky.getName());
+    }
 }
