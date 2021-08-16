e3d0ff505ad040fdece9629d267b261007d86eae
ericwafula <eric.wafula@moringaschool.com>
2021-08-16 14:36:19 +0300

    updated tests format and added location to constructor

--- src/main/java/Sighting.java
+++ src/main/java/Sighting.java
@@ -7,20 +7,30 @@ import java.util.Timer;
 
 public class Sighting {
     private int id;
-    private String name;
+    private String rangerName;
     private int animalId;
+    private String location;
 
-    public Sighting(String name, int animalId) {
-        this.name = name;
+    public Sighting(int animalId, String location, String rangerName) {
+        this.rangerName = rangerName;
         this.animalId = animalId;
+        this.location = location;
     }
 
-    public String getName() {
-        return name;
+    public String getRangerName() {
+        return rangerName;
+    }
+
+    public int getAnimalId() {
+        return animalId;
     }
 
     public int getId() {
-        return animalId;
+        return id;
+    }
+
+    public String getLocation() {
+        return location;
     }
 
     @Override
@@ -28,13 +38,35 @@ public class Sighting {
         if (this == o) return true;
         if (!(o instanceof Sighting)) return false;
         Sighting sighting = (Sighting) o;
-        return getId() == sighting.getId() &&
+        return getAnimalId() == sighting.getAnimalId() &&
                 animalId == sighting.animalId &&
-                Objects.equals(getName(), sighting.getName());
+                Objects.equals(getRangerName(), sighting.getRangerName());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getId(), getName(), animalId);
+        return Objects.hash(getAnimalId(), getRangerName(), animalId);
+    }
+
+
+    public void save(){
+        try(Connection con = DB.sql2o.open()){
+            String sql = "INSERT INTO sightings (animalid, location, rangername) VALUES (:animalId, :location, :rangerName)";
+            this.id = (int) con.createQuery(sql, true)
+                    .addParameter("animalId", this.animalId)
+                    .addParameter("rangerName", this.rangerName)
+                    .addParameter("location", this.location)
+                    .addParameter("rangerName", this.rangerName)
+                    .executeUpdate()
+                    .getKey();
+        }
+    }
+
+    public static List<Sighting> all(){
+        try(Connection con = DB.sql2o.open()){
+            String sql = "SELECT * FROM sightings";
+            return con.createQuery(sql)
+                    .executeAndFetch(Sighting.class);
+        }
     }
 }
--- src/test/java/SightingTest.java
+++ src/test/java/SightingTest.java
@@ -7,27 +7,34 @@ import static org.junit.Assert.assertTrue;
 public class SightingTest {
     @Test
     public void sighting_instantiatesCorrectly_true(){
-        Sighting animalSighting = new Sighting("Becky", 1);
+        Sighting animalSighting = new Sighting(1, "Zone A", "Becky");
         assertTrue(animalSighting instanceof Sighting);
     }
 
     @Test
     public void name_getSName_true() {
-        Sighting animalSighting = new Sighting("Becky", 1);
-        assertEquals("Becky", animalSighting.getName());
+        Sighting animalSighting = new Sighting(1, "Zone A", "Becky");
+        assertEquals("Becky", animalSighting.getRangerName());
     }
 
     @Test
     public void animal_getsAnimalId() {
-        Sighting animalSighting = new Sighting("Becky", 1);
+        Sighting animalSighting = new Sighting(1, "Zone A", "Becky");
         assertEquals(1, animalSighting.getId());
     }
 
     @Test
     public void equals_nameAndAnimalIdAreEqual() {
-        Sighting animalSighting = new Sighting("Becky", 1);
-        Sighting animalSightingTwo = new Sighting("Becky", 1);
+        Sighting animalSighting = new Sighting(1, "Zone A", "Becky");
+        Sighting animalSightingTwo = new Sighting(1, "Zone A", "Becky");
         assertTrue(animalSighting.equals(animalSightingTwo));
+    }
+
+    @Test
+    public void save_returnsTrueIfDescriptionsAreTheSame() {
+        Sighting animalSighting = new Sighting(1, "Zone A", "Becky");
+        animalSighting.save();
+        assertTrue(Sighting.all().get(0).equals(animalSighting));
     }
 
 
