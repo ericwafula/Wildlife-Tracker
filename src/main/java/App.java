8788ddb64cfabf9c4477b4b44aac3186a83299a8
ericwafula <eric.wafula@moringaschool.com>
2021-08-17 08:53:34 +0300

    added default route and form

---
+++ src/main/java/App.java
@@ -0,0 +1,18 @@
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import static spark.Spark.*;
+
+public class App {
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+    }
+}
